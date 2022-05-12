b27036046b190061651ab9349c3f39f1fdfc75ff
顾悦 <guyue@ovopark.com>
2022-05-12 17:19:01 +0800

    1

---
+++ src/main/java/xyz/klausturbo/manageable/kafka/consumer/ManageableKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package xyz.klausturbo.manageable.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageableKafkaConsumerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ManageableKafkaConsumerApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/xyz/klausturbo/manageable/kafka/consumer/ManageableKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.klausturbo.manageable.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageableKafkaConsumerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
